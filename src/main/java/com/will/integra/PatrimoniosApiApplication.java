f788d8676f22e191140f736ef126f5ee526c00af
WillikinsM <willikins98@gmail.com>
2021-07-22 20:47:03 -0300

    commit inicial

---
+++ src/main/java/com/will/integra/PatrimoniosApiApplication.java
@@ -0,0 +1,13 @@
+package com.will.integra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimoniosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrimoniosApiApplication.class, args);
+	}
+
+}
